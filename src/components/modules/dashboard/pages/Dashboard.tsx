65454751ef4cd1c41b477c0f58dc5c558a53f55d
FabianSanchezD <fabiansanchezd@outlook.com>
2025-11-20 20:50:07 -0300

    fix: padding was too high

--- src/components/modules/dashboard/pages/Dashboard.tsx
+++ src/components/modules/dashboard/pages/Dashboard.tsx
@@ -6,7 +6,7 @@ import AssetBreakdown from "../ui/AssetBreakdown";
 const Dashboard: React.FC = () => {
   return (
     <div>
-      <div className="container mx-auto px-4 py-8">
+      <div className="container mx-auto px-4 py-2">
         <MainStats />
         <GraphicStats />
         <AssetBreakdown />
